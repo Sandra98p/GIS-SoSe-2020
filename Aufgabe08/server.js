95112a7c51b36eb8a9e5c3b58470b80b15aed45b
Sandra98p <64126420+Sandra98p@users.noreply.github.com>
2020-06-20 20:59:56 +0200

    Änderung

--- Aufgabe08/server.js
+++ Aufgabe08/server.js
@@ -4,23 +4,27 @@ exports.A08Server = void 0;
 const Http = require("http");
 var A08Server;
 (function (A08Server) {
-    console.log("Starting server"); //Server wird gestartet
-    let port = Number(process.env.PORT);
+    console.log("Starting server"); //Server wird gestartet und in der Konsole ausgegeben
+    let port = Number(process.env.PORT); //Port erstellen
     if (!port) //Port ist nicht vorhanden bzw. nicht gefunden, dann ...
-        port = 8100; //setze ihn auf den Wert 8100
-    //Variable wird erstellt
+        port = 8100; //...setze ihn auf den Wert 8100
+    //Variable server wird erstellt
     let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest); //Listener erstellen und zuweisen
+    server.addListener("listening", handleListen); //Listener erstellen und zuweisen
     server.listen(port);
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); //Konsolenausgabe
     }
+    //Funktion erzeugen, "I hear voices" wird in der Konsole ausgegeben
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
+        //Kopf und Parameter erstellen
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Serverantwort url
         _response.write(_request.url);
+        //Ende der Nachricht
         _response.end();
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
